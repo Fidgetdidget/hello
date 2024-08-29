cb362f2eec35365acc3c515254482964ef026e5c
Parker <ppoehling@outlook.com>
2024-08-29 10:12:57 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,3 +1,9 @@
 
-    printf("Computer Engineering\n");
+#include <stdlib.h>
+#include <stdio.h>
 
+int main(int argc, char **argv) {
+
+    printf("Parker Poehling!\n");
+    printf("Computer Engineering\n");
+}
